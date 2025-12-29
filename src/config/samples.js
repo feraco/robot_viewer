b6137b127d5c5002ac3133a307be1c3191196a94
Fred Feraco <frederick.feraco@gmail.com>
2025-12-29 07:25:14 -0500

    Added car_turn_right.csv

--- src/config/samples.js
+++ src/config/samples.js
@@ -8,6 +8,11 @@ export const SAMPLE_MODELS = [
     name: 'Unitree G1 (23 DOF)',
     url: './g1/g1_23dof.xml',
     type: 'xml'
+  },
+  {
+    name: 'Simple Car',
+    url: './car/car.xml',
+    type: 'xml'
   }
 ];
 
@@ -51,5 +56,25 @@ export const SAMPLE_MOTIONS = [
     name: 'G1 Dance 1',
     url: 'https://huggingface.co/datasets/lvhaidong/LAFAN1_Retargeting_Dataset/resolve/main/g1/dance1_subject1.csv',
     robotType: 'G1'
+  },
+  {
+    name: 'Car Forward',
+    url: './car_forward.csv',
+    robotType: 'Car'
+  },
+  {
+    name: 'Car Backward',
+    url: './car_backward.csv',
+    robotType: 'Car'
+  },
+  {
+    name: 'Car Turn Left',
+    url: './car_turn_left.csv',
+    robotType: 'Car'
+  },
+  {
+    name: 'Car Turn Right',
+    url: './car_turn_right.csv',
+    robotType: 'Car'
   }
 ];
